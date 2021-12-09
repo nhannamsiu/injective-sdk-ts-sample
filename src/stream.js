b79fa5be1dd035e0a233251ac1a9e0905d7148ca
nhannamsiu <dangduynam95@gmail.com>
2021-12-09 18:43:14 +0700

    Add stream example

---
+++ src/stream.js
@@ -0,0 +1,31 @@
+const { DerivativeMarketConsumer, DerivativeMarketStream } = require('@injectivelabs/derivatives-consumer')
+
+const derivativeMarketStream = new DerivativeMarketStream('https://public.api.injective.network')
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  streams = []
+  for (i=0; i<1; i++) {
+    try{
+      stream = derivativeMarketStream.orderbook.start({
+        marketIds: ['0x4ca0f92fc28be0c9761326016b5a1a2177dd6375558365116b5bdda9abc229ce'],
+        callback: (res) => {
+          console.log(res)
+        },
+        onEndCallback: () => {
+          console.log(`stream ended`)
+        },
+      })
+      console.log("started stream",i)
+      streams.push(stream)
+    }
+    catch(err) {
+      console.log(err)
+    }
+
+  }
+}
+main()
